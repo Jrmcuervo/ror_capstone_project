543103fb247b3827a2f1a402f203367fa6579291
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-16 16:47:42 -0600

    Add foreign key to categories

---
+++ db/migrate/20230916224614_add_foreign_key_to_categories.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToCategories < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :categories, :users, column: :author_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_224254) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_224614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_16_224254) do
     t.string "photo"
   end
 
+  add_foreign_key "categories", "users", column: "author_id"
 end
