34282fd8ed4e34da05c1c87347754fb17acaa799
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-16 15:25:20 -0600

    Update files

--- db/migrate/20230916211423_add_devise_to_users.rb
+++ db/migrate/20230916211423_add_devise_to_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[7.0]
-  def change
+class AddDeviseToUsers < ActiveRecord::Migration[7.0]
+  def self.up
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -22,10 +22,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -33,7 +33,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      # Uncomment below if timestamps were not included in your original model.
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
@@ -41,4 +42,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
+
+
 end
