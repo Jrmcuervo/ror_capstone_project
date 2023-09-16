a8be06003fda7c2608c9b2ed134ab6bb8d75ecd0
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-16 16:18:34 -0600

    Link author to categories

---
+++ db/migrate/20230916220707_drop_abilities.rb
@@ -0,0 +1,5 @@
+class DropAbilities < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :abilities
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_215245) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_220748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "abilities", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
   create_table "entities", force: :cascade do |t|
