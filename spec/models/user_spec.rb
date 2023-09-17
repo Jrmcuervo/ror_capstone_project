7c75156eb3320898f5c8493380880caf94f016c6
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-16 21:03:23 -0600

    Build user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'User1', email: 'test2@mail.com', password: '123456')
+  end
+  before { subject.save }
+
+  it 'name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be empty' do
+    subject.name = ''
+    expect(subject).to_not be_valid
+  end
+
+  it 'should valid the name' do
+    subject.name = 'User1'
+    expect(subject).to be_valid
+  end
 end
