ae524d33d9e38032418581b15175d08850d3a263
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-16 21:08:06 -0600

    Build RSpec tests for GroupTrack's new payment path

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Payments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'GroupTrack', type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { User.create(name: 'User1', email: 'test@mail.com', password: '1234') }
+
+  describe 'GET /new' do
+    before do
+      sign_in user
+      get new_payment_path
+    end
+    it 'should return response status correct (ok)' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'respons to html' do
+      expect(response.content_type).to include 'text/html'
+    end
+    it 'should include correct placeholder' do
+      expect(response.body).to include('Name')
+    end
+    it 'should include correct placeholder' do
+      expect(response.body).to include('Amount')
+    end
   end
 end
